6359b7138188acd9ce7779c57b2f4fe0c077b9b2
with-heart <with.heart+git@pm.me>
2022-01-29 11:56:40 -0500

    init repo

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>7GUIs—XState</title>
+        <meta name="description" content="XState-based solutions for 7GUIs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>7GUIs</main>
+    </div>
+  )
+}
+
+export default Home
