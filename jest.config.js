644e1e546b73625d1050806861a1656a44186ec9
with-heart <with.heart+git@pm.me>
2022-01-29 12:01:04 -0500

    set up next/jest+testing-library

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+module.exports = createJestConfig({
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+})
