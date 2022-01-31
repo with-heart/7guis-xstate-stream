b820489ee4958238a70f3cef1551d0a5b022ecd1
with-heart <with.heart+git@pm.me>
2022-01-31 10:37:51 -0500

    add solution to task 1

--- guis/1-counter.test.tsx
+++ guis/1-counter.test.tsx
@@ -1,3 +1,27 @@
-test.todo('initial value is 0')
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Counter} from './1-counter'
 
-test.todo('clicking "Count" increases the counter by 1')
+test('initial value is 0', () => {
+  render(<Counter />)
+
+  expect(screen.getByRole('textbox', {name: 'Counter'})).toHaveValue('0')
+})
+
+test('textbox is read only', () => {
+  render(<Counter />)
+
+  userEvent.type(screen.getByRole('textbox', {name: 'Counter'}), '{selectall}1')
+
+  expect(screen.getByRole('textbox', {name: 'Counter'})).toHaveValue('0')
+})
+
+test('clicking "Count" increases the counter by 1', () => {
+  render(<Counter />)
+
+  expect(screen.getByRole('textbox', {name: 'Counter'})).toHaveValue('0')
+
+  userEvent.click(screen.getByRole('button', {name: 'Count'}))
+
+  expect(screen.getByRole('textbox', {name: 'Counter'})).toHaveValue('1')
+})
--- guis/1-counter.tsx
+++ guis/1-counter.tsx
@@ -1,1 +1,39 @@
-export const Counter = () => <div>Counter</div>
+import {Button, HStack, Input} from '@chakra-ui/react'
+import {useMachine} from '@xstate/react'
+import {assign, createMachine} from 'xstate'
+
+const counterMachine = createMachine(
+  {
+    id: 'counter',
+    tsTypes: {} as import("./1-counter.typegen").Typegen0,
+    schema: {
+      context: {} as {count: number},
+      events: {} as {type: 'INCREMENT'},
+    },
+    context: {
+      count: 0,
+    },
+    on: {
+      INCREMENT: {actions: 'incrementCount'},
+    },
+  },
+  {
+    actions: {
+      incrementCount: assign({
+        count: (context) => context.count + 1,
+      }),
+    },
+  },
+)
+
+export const Counter = () => {
+  const [state, send] = useMachine(counterMachine)
+  const count = state.context.count
+
+  return (
+    <HStack>
+      <Input aria-label="Counter" value={count} isReadOnly />
+      <Button onClick={() => send('INCREMENT')}>Count</Button>
+    </HStack>
+  )
+}
